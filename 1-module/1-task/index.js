d1ffe756204a32815927bdd026ea102b07c42981
Nikita Vidosov <dangerouschebyrashka.123@gmail.com>
2024-07-15 15:33:34 +0300

    добавил переменные d, которая возвращает нужное значение при определенных условиях и sum, которая принимает нужные значения

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  let d = n , sum = 1;
+  if (n === 0 || n === 1) {
+      return 1;
+  } else {
+  for (sum; sum != n; ++sum){
+  d *= sum;
+  } 
+  }
+  return d;
 }
