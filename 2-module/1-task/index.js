0bdc407433cf19019c2e2ef6a5ff4b899cc9c264
Nikita Vidosov <dangerouschebyrashka.123@gmail.com>
2024-07-18 09:53:18 +0300

    Модуль 2: задача 1 сделал проверку значений ключей, которая проверяет является ли значение числом и яадяется ли это число конечным. Задача 2 добавил проверку на наличие ключей. Задание 3 добавил функции по получению переменных, их сложению и умжножению.

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,9 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let x = 0;
+  for (let key in salaries) {
+  if (typeof salaries[key] === "number" && isFinite(salaries[key]) == true) {
+    x += salaries[key]
+  } 
+  }
+  return x;
 }
--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,20 @@
 let calculator = {
-  // ваш код
-};
+  a: 0,
+  b: 0,       
+    read(a, b) {
+          this.a=a;
+          this.b=b; 
+          return this.a, this.b;
+         },
+     
+          sum() {
+             return this.a + this.b;
+          },
+     
+          mul() {
+             return this.a * this.b;
+          },
+}
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
