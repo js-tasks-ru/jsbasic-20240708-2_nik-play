cef54d813bdef2236187e1cab2357bd0f2a507dd
Nikita Vidosov <dangerouschebyrashka.123@gmail.com>
2024-07-30 23:25:52 +0300

    Модуль 4: задача 1 собираю полное имя из переменных имени и фамилии, создаю ul и присваиваю класс friends далее вставляю новую li с полным именем и классом friend; задача 2 добавил цикл, который красит следующую ячейку последующего столбца; задача 3 добавил проверки на значение атрибута(и на его отсутствие) и на значение ячеек таблицы.

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let fullName = friends.map(x => x.firstName + " " + x.lastName);
+  let ul = document.createElement("ul");
+  ul.classList.add('friends');
+  document.body.append(ul);
+  for (let friend of friends) {
+    ul.insertAdjacentHTML ('beforeEnd', `<li class="friend">${fullName}</li>`);
+}
+return ul
 }
