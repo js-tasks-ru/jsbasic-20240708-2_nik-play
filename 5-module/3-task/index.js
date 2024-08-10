dbc2d7ee546108a8ddd0741addd74a891a1e78fb
Nikita Vidosov <dangerouschebyrashka.123@gmail.com>
2024-08-11 00:24:33 +0300

    Модуль 6: задание 1 - через перебор массива и append, а также реализовал нажатие кнопки; задание 2 - создал две функции, которые создают карточку товара; задание 3 - добавил функцию, чтобы перебором создавалась основная часть карусели, добавил функцию для кнопок и отдельно для их работы, а также сделал задал заранее offsetWidth

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -5,7 +5,7 @@ let carousel = document.querySelector('.carousel__inner');
 let carouselWidth = carousel.offsetWidth;
 let x = 0, y = 0;
 
-arrowLeft.style.display = 'none';
+arrowLeft.style.display = 'none';  
 
 arrowRight.addEventListener('click', () => {
     x += carouselWidth;
@@ -18,12 +18,7 @@ arrowRight.addEventListener('click', () => {
         arrowLeft.style.display = '';
     }
     carousel.style.transform = `translateX(-${x + "px"})`
-    /* Подскажите пожалуйста, у меня ранее в коде здесь была строчка 
-return x, y; почему она здесь не нужна я просто не пойму... получается 
-при клике задается ведь новое значение переменной и оно должно 
-возвращаться, чтобы при клике на другую кнопку продолжалось с нового заданного числа.
-Вообщем почему не нужен return x,y?
-*/
+
     })
 
 arrowLeft.addEventListener('click', () => {
