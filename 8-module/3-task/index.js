0abe6f3a7de541770184687c85f978de4df02f32
Nikita Vidosov <dangerouschebyrashka.123@gmail.com>
2025-01-07 23:30:41 +0300

    Задание 3 - реализовал функцию, чтобы при наличии продукта он добавлялся по id и далее складывался как в количестве конкретного товара, так и в общую массу. Также было реализовано сложение общей суммы товаров.

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -3,26 +3,50 @@ export default class Cart {
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
+    this.productCount = 0;
+    this.totalPrice = 0;
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product) return;
+
+    const cartItem = this.cartItems.find(item => item.product.id === product.id);
+
+    if (cartItem) {
+      cartItem.count += 1;
+    } else {
+      this.cartItems.push({ product, count: 1 });
+    }
+    this.getTotalCount();
+    this.getTotalPrice();
+    this.onProductUpdate();
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    const cartItem = this.cartItems.find(item => item.product.id === productId);
+
+    if (cartItem) {
+      cartItem.count += amount;
+
+      if (cartItem.count <= 0) {
+        this.cartItems = this.cartItems.filter(item => item.product.id !== productId);
+      }
+    }
+    this.getTotalCount()
+    this.getTotalPrice()
+    this.onProductUpdate();
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.count, 0)
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.product.price * item.count, 0);
   }
 
   onProductUpdate(cartItem) {
